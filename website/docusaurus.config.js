3bfa13a748c7a4675f48f1475aefe1021b8d1ad7
Aresh Tajvar <33988644+AreTaj@users.noreply.github.com>
2025-12-16 19:37:08 -0800

    Fix broken links config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   // GitHub pages deployment config.
   organizationName: 'AreTaj', // Your GitHub user name.
   projectName: 'Migraine-Navigator', // Your repo name.
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
